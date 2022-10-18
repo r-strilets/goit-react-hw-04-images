a53e2217704053a7955c0700a417795ff094597d
r-strilets <str.komar@gmail.com>
2022-10-18 21:40:35 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -30,6 +30,7 @@ export const App = () => {
           setError('nothing found');
         }
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [name, page]);
 
   const newSearch = newName => {
